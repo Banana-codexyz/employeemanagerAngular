4f487d5299cf130dc7bdcfa97870e57a163fd875
Nguyen Nguyen Khoi <nknguyen@IfiSolution.local>
2022-08-12 14:47:32 +0700

    crud

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -21,7 +21,7 @@ export class EmployeeService {
   }
 
   public updateEmployee(employee: Employee): Observable<Employee>{
-    return this.http.put<Employee>(`${this.apiServerUrl}/employee/add`,employee);
+    return this.http.put<Employee>(`${this.apiServerUrl}/employee/update`,employee);
   }
 
   public deleteEmployee(employeeId: number): Observable<void>{
